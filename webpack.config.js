c4dd26dce3ac2f31964ff3d9a2f9bd29021e164a
luwenchun <luwenchun@users.noreply.github.com>
2019-04-26 17:20:32 +0800

    webpack-loader 作用是@babel/core和webpack之间沟通桥梁 @babel/preset-env把es6转化成es5 通过@babel/polyfill增加全部es6新特性的全局注入    test: /\.js$/,           exclude:/node_modules/,           loader:"babel-loader",
    通过按需加载需要解析的es6新特性
     //   options:{
            //       presets:[["@babel/preset-env",{
            //           useBuiltIns:"usage"
            //       }]]
            //   }

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,16 @@
             'postcss-loader'
         ]
       },
+      {
+          test: /\.js$/,
+          exclude:/node_modules/,
+          loader:"babel-loader",
+        //   options:{
+        //       presets:[["@babel/preset-env",{
+        //           useBuiltIns:"usage"
+        //       }]]
+        //   }
+      },
        {
         test: /\.scss$/,
         use: [
